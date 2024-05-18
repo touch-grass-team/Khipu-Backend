94ff9d9f1f05dac7e966debea0c596a3d4fbc3db
RegreTTO <atary_game@mail.ru>
2024-05-18 15:16:04 +0300

    Added file writer for logs. Root-relative paths added

--- src/config/logging.py
+++ src/config/logging.py
@@ -1,15 +1,24 @@
 import logging
 
+from src.config.path import LOG_FILE_PATH
+
 BASE_LOGGER_NAME = "main"
 FORMAT = (
     "[%(asctime)s] - [%(name)s/%(funcName)s:%(lineno)d] - [%(levelname)s] - %(message)s"
 )
 
-base_logger = logging.getLogger(BASE_LOGGER_NAME)
 
 def setup_logger():
+    formatter = logging.Formatter(FORMAT)
+
+    base_logger = logging.getLogger(BASE_LOGGER_NAME)
+    base_logger.setLevel(logging.INFO)
+
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(logging.Formatter(FORMAT))
-    base_logger.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    file_handler = logging.FileHandler(LOG_FILE_PATH)
+    file_handler.setFormatter(formatter)
+
+    base_logger.addHandler(file_handler)
     base_logger.addHandler(console_handler)
