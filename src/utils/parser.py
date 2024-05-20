80d967da3eaec7da8a59180ca4ab152bb4c4a33e
RegreTTO <atary_game@mail.ru>
2024-05-20 20:25:40 +0300

    Parsing updated

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -7,6 +7,7 @@ from typing import Optional
 @dataclass
 class LogInfo:
     Timestamp: datetime.datetime
+    Level: str
     User: str
     Process: str
     PID: Optional[int]
@@ -18,19 +19,15 @@ def log_parse(log: bytes):
     parts = list(map(str, log.partition(": ")))
     head = parts[0].split()
     month = re.search(r"[A-Za-z]+", head[0])[0]
-    day = head[1]
-    time = head[2]
-    user = head[3]
-    proccess = re.search(r"([a-zA-Z]+)", head[4])[0]
-    pid = re.search(r"\[(\d+)\]", head[4])
+    timestamp = datetime.datetime.fromisoformat(head[0])
+    level = head[1]
+    user = head[2]
+    proccess = re.search(r"([a-zA-Z]+)", head[3])[0]
+    pid = re.search(r"\[(\d+)\]", head[3])
     if pid:
         pid = pid[1]
     message = parts[2]
-    year = datetime.date.today().year
-    timestamp = datetime.datetime.strptime(
-        f"{month} {day.zfill(2)} {year} {time}", "%b %d %Y %H:%M:%S"
-    )
     log_info = LogInfo(
-        Timestamp=timestamp, User=user, Process=proccess, PID=pid, Message=message
+        Timestamp=timestamp, User=user, Process=proccess, PID=pid, Message=message, Level=level
     )
     return log_info
