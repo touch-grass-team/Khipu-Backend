f7fe608c4da15edf333274f9806fa4836b0da2b8
sa <sa@astra>
2024-05-22 00:27:15 +0300

    DB fixing!

--- install_src/scripts/sql/init.sql
+++ install_src/scripts/sql/init.sql
@@ -1,4 +1,10 @@
-﻿CREATE TABLE IF NOT EXISTS syslog_ng.logs.logs_info(
+﻿CREATE ROLE server_role;
+CREATE ROLE client_role;
+CREATE USER log_writer WITH ENCRYPTED PASSWORD 'server';
+CREATE USER log_reader WITH ENCRYPTED PASSWORD 'client';
+
+
+CREATE TABLE IF NOT EXISTS syslog_ng.logs.logs_info(
 	_id SERIAL NOT NULL PRIMARY KEY,
 	_timestamp timestamp without time zone NOT NULL,
 	_level character varying(50),
@@ -19,15 +25,15 @@ CREATE OR REPLACE FUNCTION syslog_ng.logs.prc_ins_logs_info(
 RETURNS INTEGER AS
 $BODY$
 BEGIN
-INSERT INTO syslog_ng.logs.logs_info("_timestamp","_level","_user","_process","_pid","_message")
-VALUES (new_timestamp,new_level,new_user,new_process,new_pid,new_message);
+INSERT INTO syslog_ng.logs.logs_info("_timestamp","_level","_user_name","_process_name","_pid","_message")
+VALUES (new_timestamp,new_level,new_user_name,new_process_name,new_pid,new_message);
 RETURN 0;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
-COMMENT ON FUNCTION syslog_ng.logs.prc_ins_logs_info(timestamp,character varying,character varying,character varying,integer,character varying) IS 'Вставляет данные данные о новой записи'
+COMMENT ON FUNCTION syslog_ng.logs.prc_ins_logs_info(timestamp,character varying,character varying,character varying,integer,character varying) IS 'Вставляет данные данные о новой записи';
 
 CREATE TYPE syslog_ng.logs.logs_file_info_type AS (
 	_id integer,
@@ -38,3 +44,15 @@ CREATE TYPE syslog_ng.logs.logs_file_info_type AS (
 	_pid integer,
 	_message character varying);
 
+GRANT USAGE ON SCHEMA logs TO server_role,client_role;
+GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE syslog_ng.logs.logs_info to server_role;
+GRANT SELECT ON TABLE syslog_ng.logs.logs_info to client_role;
+
+REVOKE ALL ON FUNCTION syslog_ng.logs.prc_ins_logs_info(timestamp,character varying,character varying,character varying,integer,character varying) FROM public;
+GRANT EXECUTE ON FUNCTION syslog_ng.logs.prc_ins_logs_info(timestamp,character varying,character varying,character varying,integer,character varying) TO server_role;
+
+
+GRANT SELECT,UPDATE ON SEQUENCE syslog_ng.logs.logs_info__id_seq TO server_role;
+
+GRANT server_role to log_writer;
+GRANT client_role to log_reader;
