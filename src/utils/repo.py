de66cf9b684b5efac9d21933d78ab14fa09d6efb
StepanGulyaev <stepan.gulyaev.02@gmail.com>
2024-05-22 23:00:25 +0300

    Changed server to db connect user

--- src/utils/repo.py
+++ src/utils/repo.py
@@ -1,7 +1,7 @@
 import psycopg
 
 def send_log(log_info):
-    with psycopg.connect("dbname=syslog_ng user=postgres password=postgres") as conn:
+    with psycopg.connect("dbname=syslog_ng user=log_writer password=server") as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT syslog_ng.logs.prc_ins_logs_info(%s, %s, %s, %s, %s, %s)",
                          (log_info.Timestamp.strftime('%Y-%m-%d %H:%M:%S'), log_info.Level, log_info.User, log_info.Process, log_info.PID, log_info.Message))
